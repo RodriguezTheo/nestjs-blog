2bd5e3cc5d10dc95f42d9a94a79d3550dc975b52
Theo Rodriguez <rodriguez.theo@outlook.com>
2023-12-04 01:06:06 +0100

    adding user CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { HttpAdapterHost, NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { PrismaClientExceptionFilter } from "./prisma-client-exception/prisma-client-exception.filter";
 import { useGlobalPipesHandlers } from "./handlers/useGlobalPipesHandlers";
 import { useSwaggerHandlers } from "./handlers/useSwagger.handlers";
+import { ClassSerializerInterceptor } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   useGlobalPipesHandlers(app);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   useSwaggerHandlers(app);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
