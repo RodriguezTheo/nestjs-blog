7317e3caa1916bab5542a186551d0bab5395f36b
Theo Rodriguez <rodriguez.theo@outlook.com>
2023-12-04 01:06:06 +0100

    adding user CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { PrismaModule } from './prisma/prisma.module';
-import { ArticlesModule } from './articles/articles.module';
+import { PrismaModule } from "./prisma/prisma.module";
+import { ArticlesModule } from "./articles/articles.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [PrismaModule, ArticlesModule],
+  imports: [PrismaModule, ArticlesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
